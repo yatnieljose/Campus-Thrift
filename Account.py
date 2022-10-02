0904a3eba0040023bdb8b3bd98e07592cf5ac52e
Yatniel Jose Ramos Rivera <46455372+yatnieljose@users.noreply.github.com>
2022-10-02 20:34:00 +0200

    Update Account.py

--- Account.py
+++ Account.py
@@ -1,24 +1,61 @@
 class Account:
     def __init__(self, name, email, bio="NONE"):
-            self._name = name
-            self._email = email
-            self._bio = bio
-            completedTrans = []
+            self._name = name         #str
+            self._email = email       #str
+            self._bio = bio           #str
+            self._rank = 1            #int
+            self._profilePicture = "" #str of the picture path, default needed
+            self._numRejected = 0     #int
+            self._completedTrans = [] #list
+            self._listedItems = []    #list
             
     @property
-    def name(self):
+    def getName(self):
         return(self._name)
-    def email(self):
-        return(self._email)
-    def bio(self):
-        return(self._bio)
-    
-    @name.setter
-    def name(self, newName):
+    @getName.setter
+    def setName(self, newName):
         self._name = newName
-    @email.setter
-    def email(self, newEmail):
+        
+    @property
+    def getEmail(self):
+        return(self._email)
+    @getEmail.setter
+    def setEmail(self, newEmail):
         self._email = newEmail
-    @bio.setter
-    def bio(self, newBio):
+        
+    @property
+    def getBio(self):
+        return(self._bio)
+    @getBio.setter
+    def setBio(self, newBio):
         self._bio = newBio
+        
+    @property
+    def getRank(self):
+        return(self._rank)
+    @getRank.setter
+    def setRank(self, newRank):
+        self._rank = newRank
+        
+    @property
+    def getProfilePicture(self):
+        return(self._profilePicture)
+    @getProfilePicture.setter
+    def setProfilePicture(self, newPicture):
+        self._profilePicture = newPicture
+        
+    @property
+    def numOfRejected(self):
+        return(self._numRejected)
+    
+    def incNumOfRejected(self):
+        self._numRejected += 1
+    @numOfRejected.setter #needed?
+    def decNumOfRejected(self):
+        if numRejected != 0:
+            self._numRejected -= 1
+            
+    @property
+    def numOfCompleted(self):
+        return(len(self._completedTrans))
+    
